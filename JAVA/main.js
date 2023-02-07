d805a5424ff2b5cb19ad97f629d46b7aca4c4842
Facundo <facuvallejo00@gmail.com>
2023-02-07 19:42:28 -0300

    se termina de arreglar calculos

--- JAVA/main.js
+++ JAVA/main.js
@@ -1,25 +1,25 @@
 function limpiar() {
     document.getElementById('formulario').reset();
   }
-  
+
+          //DOLAR - EURO - REAL//
+let precios = [325.74,349.09,38];
+
 
   function dolar() {
   var x = parseInt(document.getElementById('valor1').value);
-  var y = Number(325,74);
-  var suma = (x*y);
+  var suma = (x*precios[0]);
   document.getElementById('el-resultado').innerHTML = suma;
   }
 
   function euro() {
     var x = parseInt(document.getElementById('valor1').value);
-    var y = Number(349,09);
-    var suma = (x*y);
+    var suma = (x*precios[1]);
     document.getElementById('el-resultado').innerHTML = suma;
     }
 
     function real() {
         var x = parseInt(document.getElementById('valor1').value);
-        var y = Number(38);
-        var suma = (x*y);
+        var suma = (x*precios[2]);
         document.getElementById('el-resultado').innerHTML = suma;
     }
